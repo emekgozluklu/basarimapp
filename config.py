f407c3c6ebd7821cf0c63a0f3047580715a49458
Emek Gözlüklü <emekgzlkl@gmail.com>
2020-12-22 21:17:35 +0300

    initialize database

---
+++ config.py
@@ -0,0 +1,49 @@
+import os
+from configparser import ConfigParser
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+def db_config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
---
+++ database_init.py
@@ -0,0 +1,46 @@
+from configparser import ConfigParser
+import psycopg2
+
+DB_INITIAL_QUERY = open("db_structure.sql", "r").read()
+TABLE_NAMES = ["USERROLE", "EXAM", "EXAMFIELD", "RESULT", "FIELDRESULT", "ANSWERSHEET"]
+
+
+def get_params(filename, section):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            "Section {0} not found in the {1} file".format(section, filename)
+        )
+    return db
+
+
+def init_db(filename="database.ini", section="postgresql"):
+
+    db = get_params(filename, section)
+    with psycopg2.connect(**db) as conn:
+        cur = conn.cursor()
+        cur.execute(DB_INITIAL_QUERY)
+    cur.close()
+
+
+def del_db(filename="database.ini", section="postgresql"):
+
+    db = get_params(filename, section)
+    with psycopg2.connect(**db) as conn:
+        cur = conn.cursor()
+        for table in TABLE_NAMES:
+            cur.execute(f"DROP TABLE IF EXISTS {table} CASCADE")
+    cur.close()
+
+
+if __name__ == "__main__":
+    # del_db()
+    init_db()
